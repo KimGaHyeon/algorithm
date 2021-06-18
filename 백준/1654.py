57c1de7a3a5be8a3459612b4ae041e749a7b852f
Kimgahyeon <kimgahyun1@ajou.ac.kr>
2021-06-18 21:23:08 +0900

    백준 문제 추가

---
+++ 백준/1654.py
@@ -0,0 +1,19 @@
+# https://www.acmicpc.net/problem/1654
+
+import sys
+sys.stdin = open('input/1654.txt','r')
+
+K, N = map(int, input().split())
+lan = [int(sys.stdin.readline()) for _ in range(K)]
+start, end = 1, max(lan)
+
+while start <= end:
+    mid = (start+end)//2
+    count = 0
+    for i in lan:
+        count += i // mid
+    if count >= N:
+        start = mid + 1
+    else:
+        end = mid - 1
+print(end)
