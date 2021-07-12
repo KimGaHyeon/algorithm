9dbf73b26560be91f7d6ac80a9ec118c6883cf28
Kimgahyeon <kimgahyun1@ajou.ac.kr>
2021-07-12 16:13:45 +0900

    문제 추가

--- programmers/2018 KAKAO BLIND RECRUITMENT/17681.py
+++ programmers/2018 KAKAO BLIND RECRUITMENT/17681.py
@@ -1,13 +1,20 @@
 # https://programmers.co.kr/learn/courses/30/lessons/17681
-# def solution(n, arr1, arr2):
-#     answer = []
+n=6
+arr1=[46, 33, 33 ,22, 31, 50]
+arr2=[27 ,56, 19, 14, 14, 10]
 
-n=5
-arr1=[9, 20, 28, 18, 11]
-arr2=[30, 1, 21, 17, 28]
+def solution(n,arr1,arr2):
+    arr1_bin = list(i[2:] for i in map(bin, arr1))
+    arr2_bin = list(i[2:] for i in map(bin, arr2))
+    answer = []
+    for a,b in (zip(arr1_bin, arr2_bin)):
+        answer.append(int(a)+int(b))
 
-bin(arr1[1])
-# print(solution(n,arr1,arr2))
+    answer = list(map(str, answer))
+    answer = list(i.zfill(n) for i in answer)
 
+    for i in range(n):
+        answer[i] = answer[i].replace("1","#").replace("2","#").replace("0"," ")
+    return answer
 
-
+print(solution(n,arr1,arr2))
